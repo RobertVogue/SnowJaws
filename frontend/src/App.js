e92efef1966a34eb3b4de56e4ed8d69e1fb3b647
Robert Vogtritter <RCVogtritter@gmail.com>
2021-02-24 10:04:57 -0700

    ready to begin frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,26 +3,14 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
-// import Booking from "./components/MVP/Booking";
-// import Map from "./components/MVP/Map";
-// import Review from './components/MVP/Review';
-// import Spot from './components/MVP/Spot'
-
-
-
 import * as sessionActions from "./store/session";
-import * as spotStuff from './store/spot'
+
 import Navigation from "./components/Navigation";
 import './index.css'
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    const withReviews = false;
-    dispatch(spotStuff.getAllS(withReviews));
-  }, [dispatch]);
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
